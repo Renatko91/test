68b132697ebb6f58413d3cb4a3dc5d1d38072d0e
lopatnikov <r@a.ru>
2021-02-16 15:03:55 +0300

    add update

--- src/main/java/sprint/test/controller/UserController.java
+++ src/main/java/sprint/test/controller/UserController.java
@@ -3,11 +3,12 @@ package sprint.test.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import sprint.test.entity.User;
 import sprint.test.repository.UserRepository;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -17,8 +18,22 @@ public class UserController {
 
     @GetMapping("/")
     public String greeting(Model model) {
-        String s = "Rinat";
-        model.addAttribute("r", s);
+        List<User> users = userRepository.getAll();
+        model.addAttribute("users", users);
         return "index";
+    }
+
+    @PostMapping("/create")
+    public String add(@RequestParam String user_name, Model model) {
+        User user = new User();
+        user.setName(user_name);
+        userRepository.addUser(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam String del_user, Model model) {
+        userRepository.delUser(del_user);
+        return "redirect:/";
     }
 }
--- src/main/java/sprint/test/repository/UserRepository.java
+++ src/main/java/sprint/test/repository/UserRepository.java
@@ -2,12 +2,18 @@ package sprint.test.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import sprint.test.entity.User;
 import sprint.test.mapper.UserMapper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class UserRepository {
@@ -17,7 +23,17 @@ public class UserRepository {
 
     public List<User> getAll() {
         List<User> userNameList = new ArrayList<>();
-        userNameList.addAll(jdbcTemplate.query("select * from user_n;", new UserMapper()));
+        userNameList.addAll(jdbcTemplate.query("select * from user_n", new UserMapper()));
+        userNameList.sort((a, b) -> (int) (a.getId() - b.getId()));
         return userNameList;
+    }
+
+    @Transactional
+    public void addUser(User user) {
+        jdbcTemplate.update("insert into user_n (name) values (?)", user.getName());
+    }
+
+    public void delUser(String del_user) {
+        jdbcTemplate.update("delete from user_n where name = ?", del_user);
     }
 }
